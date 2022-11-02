c04334d8a7ca7448ef010367a15029efbc524e32
tuna-yapakci <tyapak@yahoo.com>
2022-11-02 15:56:44 +0100

    test5

--- loader.sh
+++ loader.sh
@@ -3,10 +3,10 @@ module="driver"
 device="custom_gpio_dev"
 mode="664"
 
-/sbin/insmod ./${module}.ko || exit 1
+/sbin/insmod ${module}.ko gpio_pin_number=21 || exit 1
 
 rm -f /dev/${device}
 
 major=`cat /proc/devices | awk "{if(\\$2==\"$device\")print \\$1}"`
 
-echo $major
+mknod /dev/${device} c $major 0
