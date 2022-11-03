c8173c488fa5e7eea4973f70c7eb6f4714c8d1ae
tuna-yapakci <tyapak@yahoo.com>
2022-11-03 18:51:32 +0100

    test8

--- driver.c
+++ driver.c
@@ -58,9 +58,8 @@ static void signal_to_pid_datarecv(void){ // change type maybe
     struct kernel_siginfo info;
     memset(&info, 0, sizeof(struct siginfo));
     info.si_signo = SIGDATARECV;
-    //printk("PID: %d\n", task->pid);
-    if (registered_process != -1) {
-        printk(KERN_WARNING "Error sending data receive signal\n");
+    if (registered_process < 0) {
+        printk(KERN_INFO "Error sending data receive signal\n");
         /*
         if(send_sig_info(SIGDATARECV, &info, task) < 0) {
             printk(KERN_WARNING "Error sending data receive signal\n");
