1475d97994b08b82aa67f284c876f7045e98658f
tuna-yapakci <tyapak@yahoo.com>
2022-11-06 14:25:17 +0100

    test11

--- driver.c
+++ driver.c
@@ -299,7 +299,7 @@ static ssize_t gpio_write(struct file *filp, const char __user *buff, size_t cou
         tmp.buffer[i] = msg[i];
     }
     tmp.length = count;
-    printk("%d\n", tmp.length);
+    printk("%s\n", tmp.buffer);
     if (data_push(&queue_to_send, tmp) < 0) {
         printk(KERN_WARNING "Queue is full, write failed\n");
         return -1;
